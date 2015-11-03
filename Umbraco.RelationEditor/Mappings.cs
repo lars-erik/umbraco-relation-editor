02408c748895876aa17716e57f259f8df12c5a41
jagbarcelo <jagbarcelo@users.noreply.github.com>
2015-11-03 12:50:09 +0100

    Update Mappings.cs
    Fix for Umbraco 7.3.x

--- Umbraco.RelationEditor/Mappings.cs
+++ Umbraco.RelationEditor/Mappings.cs
@@ -9,6 +9,7 @@ static internal class Mappings
     {
         { new TreeNodeType(null, null), UmbracoObjectTypes.Document },
         { new TreeNodeType("content", null), UmbracoObjectTypes.Document },
+        { new TreeNodeType("content", "content"), UmbracoObjectTypes.Document }, // Fix for Umbraco 7.3.x
         { new TreeNodeType("media", null), UmbracoObjectTypes.Media },
         { new TreeNodeType("settings", "nodeTypes"), UmbracoObjectTypes.DocumentType },
         { new TreeNodeType("settings", "mediaTypes"), UmbracoObjectTypes.MediaType }
